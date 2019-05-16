01f89a7e768bc75fb317f17d2d6c15670fe84fef
L-Hyunwoo <48970480+L-Hyunwoo@users.noreply.github.com>
2019-05-17 02:12:05 +0900

    Update 1week_5P.c

--- 1week_5P.c
+++ 1week_5P.c
@@ -1,22 +1,25 @@
+/*1 ~ 10 사이의 어떤 수로도 나누어 떨어지는 가장 작은 수는 2520입니다.
+그러면 1 ~ 20 사이의 어떤 수로도 나누어 떨어지는 가장 작은 수는 얼마입니까?*/
+
 #include <stdio.h>
 
 int main(void)
 {
 
-	int num = 1, i; //���� �Է�
+	int num = 1, i; //값들 입력
 
-	while (1) //������ �����ʰ� ���� �ݺ�
+	while (1) //제한을 두지않고 무한 반복
 	{
-		for (i = 1; i <= 20; i++) //1���� 20���� �ݺ�
+		for (i = 1; i <= 20; i++) //1부터 20까지 반복
 		{
-			if (num % i != 0) //num�� 1���� 20������ �� �� �ϳ��� ���������� ������
-				break; //��������
+			if (num % i != 0) //num이 1에서 20까지의 수 중 하나로 나누어지지 않으면
+				break; //수행종료
 		}
-		if (i == 21) //i�� 21�� �ȴٸ�
-			break;  //��������
+		if (i == 21) //i가 21이 된다면
+			break;  //수행종료
 		num++;
 	}
 
-	printf("%d\n", num); //num�� ���
+	printf("%d\n", num); //num값 출력
 	return 0;
 }
